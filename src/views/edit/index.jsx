0ff49ba5e72e4f47bd2bbf925d79d2bc74f8c90e
Numan Tahir <numan.tahir@novatoresols.com>
2025-03-06 23:43:27 +0500

    Update

--- src/views/edit/index.jsx
+++ src/views/edit/index.jsx
@@ -17,7 +17,7 @@ const EditProfile = () => {
         //alert(' > ' + response.data.cover_image);
         // alert(JSON.stringify(response.data));
         if (response.data) {
-            setData(response.data);
+            setData(response.data.data);
         }
         
     }
